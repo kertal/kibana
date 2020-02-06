bf729ca449b7e4fab0ae5d6ffcea1a7d50b4bcab
Matthias Wilhelm <matthias.wilhelm@elastic.co>
2020-02-06 06:47:14 +0100

    unskip test, describe.only for testing

--- test/functional/apps/visualize/_tsvb_chart.ts
+++ test/functional/apps/visualize/_tsvb_chart.ts
@@ -27,7 +27,8 @@ export default function({ getService, getPageObjects }: FtrProviderContext) {
   const inspector = getService('inspector');
   const PageObjects = getPageObjects(['visualize', 'visualBuilder', 'timePicker', 'visChart']);
 
-  describe('visual builder', function describeIndexTests() {
+  // eslint-disable-next-line ban/ban
+  describe.only('visual builder', function describeIndexTests() {
     this.tags('smoke');
     beforeEach(async () => {
       await PageObjects.visualize.navigateToNewVisualization();
@@ -116,7 +117,7 @@ export default function({ getService, getPageObjects }: FtrProviderContext) {
         await esArchiver.unload('kibana_sample_data_flights');
       });
       // FLAKY: https://github.com/elastic/kibana/issues/43150
-      it.skip('should be able to switch between index patterns', async () => {
+      it('should be able to switch between index patterns', async () => {
         const value = await PageObjects.visualBuilder.getMetricValue();
         expect(value).to.eql('156');
         await PageObjects.visualBuilder.clickPanelOptions('metric');

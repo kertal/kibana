5f581c11385a1535819163ca4ce09cd1a9b9f55e
Matthias Wilhelm <matthias.wilhelm@elastic.co>
2020-03-19 11:55:23 +0100

    unskip discover spaces

--- x-pack/test/functional/apps/discover/feature_controls/discover_spaces.ts
+++ x-pack/test/functional/apps/discover/feature_controls/discover_spaces.ts
@@ -25,7 +25,7 @@ export default function({ getPageObjects, getService }: FtrProviderContext) {
   }
 
   // FLAKY: https://github.com/elastic/kibana/issues/60559
-  describe.skip('spaces', () => {
+  describe('spaces', () => {
     before(async () => {
       await esArchiver.loadIfNeeded('logstash_functional');
     });
